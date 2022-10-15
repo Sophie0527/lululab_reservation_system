4812586b660c8d5d43834be9b9e551827e8c20b4
손소희 <ssh30510044@gmail.com>
2022-10-15 13:32:34 +0900

    Add: 예약리스트페이지, 캘린더가 들어갈 임시 위치 지정

--- src/pages/ReservationList.js
+++ src/pages/ReservationList.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import Reservation from './Reservation';
+import logo from '../images/lululab_logo.png';
 
 const ReservationList = () => {
   const [reservationOpen, setReservationOpen] = useState(false);
@@ -10,16 +11,45 @@ const ReservationList = () => {
 
   return (
     <Container>
-      Reservation List
-      {/* 캘린더 날짜 클릭 시 예약 페이지 (Reservation) 모달로 구현 */}
-      <button onClick={openReservation}>14일</button>
-      {reservationOpen && (
-        <Reservation setReservationOpen={setReservationOpen} />
-      )}
+      <ContainerBox>
+        <header>
+          <img src={logo} alt="logo" />
+        </header>
+        {/* 캘린더 날짜 클릭 시 예약 페이지 (Reservation) 모달로 구현 */}
+        <CalendarBox>
+          <button onClick={openReservation}>14일</button>
+        </CalendarBox>
+
+        {reservationOpen && (
+          <Reservation setReservationOpen={setReservationOpen} />
+        )}
+      </ContainerBox>
     </Container>
   );
 };
 
 export default ReservationList;
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+const ContainerBox = styled.div`
+  padding: 20px;
+  margin-top: 50px;
+  background-color: white;
+  header {
+    display: flex;
+    justify-content: center;
+    padding-bottom: 10px;
+  }
+  h1 {
+    display: flex;
+    justify-content: center;
+    padding-bottom: 2px;
+  }
+`;
+const CalendarBox = styled.div`
+  width: 350px;
+  margin: 0;
+`;
