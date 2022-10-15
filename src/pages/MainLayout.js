2f1ee182e8a2581e6a97e885fbf5d1e75b15aeb4
손소희 <ssh30510044@gmail.com>
2022-10-15 15:25:54 +0900

    fix: 메인 레이아웃에서 header의 버튼 클릭 시 예약하기와 예약조회페이지로 이동하도록 수정

---
+++ src/pages/MainLayout.js
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+import { Breadcrumb, Layout, Menu } from 'antd';
+import ReservationList from './ReservationList';
+import ReservationHistory from './ReservationHistory';
+import styled from 'styled-components';
+import 'antd/dist/antd.css';
+import { CalendarOutlined, HistoryOutlined } from '@ant-design/icons';
+
+const { Header, Content, Footer } = Layout;
+function getItem(label, key, icon, children, type) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  };
+}
+
+const items = [
+  getItem('예약하기', '1', <CalendarOutlined />),
+  getItem('예약조회', '2', <HistoryOutlined />),
+];
+
+const MainLayout = () => {
+  const [page, setPage] = useState('1');
+  const onClick = e => {
+    setPage(e.key);
+  };
+
+  return (
+    <Layout>
+      <Header>
+        <a href="/">
+          <Logo>
+            <h1>lululab</h1>
+          </Logo>
+        </a>
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={page}
+          items={items}
+          onClick={onClick}
+        />
+      </Header>
+      <Content
+        style={{
+          padding: '0 50px',
+        }}
+      >
+        <Breadcrumb
+          style={{
+            margin: '16px 0',
+          }}
+        >
+          <Breadcrumb.Item>룰루랩 예약 시스템</Breadcrumb.Item>
+          <Breadcrumb.Item>
+            {page === '1' ? 'Reservation' : 'Reservation History'}
+          </Breadcrumb.Item>
+        </Breadcrumb>
+        <div className="site-layout-content">
+          {page === '1' && <ReservationList />}
+          {page === '2' && <ReservationHistory />}
+        </div>
+      </Content>
+      <Footer
+        style={{
+          textAlign: 'center',
+        }}
+      >
+        13 Team
+      </Footer>
+    </Layout>
+  );
+};
+export default MainLayout;
+
+const Logo = styled.div`
+  float: left;
+  width: 120px;
+  height: 31px;
+  margin: 16px 24px 16px 0;
+  @media (max-width: 530px) {
+    width: 50px;
+    margin: 16px 10px;
+  }
+  h1 {
+    font-size: 25px;
+    line-height: 31px;
+    text-align: center;
+    font-weight: 700;
+    color: white;
+    letter-spacing: 2px;
+    @media (max-width: 530px) {
+      font-size: 17px;
+      letter-spacing: 0px;
+    }
+  }
+`;
--- src/pages/ReservationList.js
+++ src/pages/ReservationList.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import Reservation from './Reservation';
-import logo from '../images/lululab_logo.png';
 
 const ReservationList = () => {
   const [reservationOpen, setReservationOpen] = useState(false);
@@ -12,14 +11,9 @@ const ReservationList = () => {
   return (
     <Container>
       <ContainerBox>
-        <header>
-          <img src={logo} alt="logo" />
-        </header>
-        {/* 캘린더 날짜 클릭 시 예약 페이지 (Reservation) 모달로 구현 */}
         <CalendarBox>
           <button onClick={openReservation}>14일</button>
         </CalendarBox>
-
         {reservationOpen && (
           <Reservation setReservationOpen={setReservationOpen} />
         )}
@@ -34,22 +28,5 @@ const Container = styled.div`
   display: flex;
   justify-content: center;
 `;
-const ContainerBox = styled.div`
-  padding: 20px;
-  margin-top: 50px;
-  background-color: white;
-  header {
-    display: flex;
-    justify-content: center;
-    padding-bottom: 10px;
-  }
-  h1 {
-    display: flex;
-    justify-content: center;
-    padding-bottom: 2px;
-  }
-`;
-const CalendarBox = styled.div`
-  width: 350px;
-  margin: 0;
-`;
+const ContainerBox = styled.div``;
+const CalendarBox = styled.div``;
